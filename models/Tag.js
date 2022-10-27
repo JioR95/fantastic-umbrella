860831689c4a4b877f66cfc4f64352bab3931af6
JioR95 <jiovani.rivera@rocketmail.com>
2022-10-27 13:09:48 -0400

    tags colums added

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-   // define colums
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
